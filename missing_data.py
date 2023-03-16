073b83bcfeee1f808ce786d736d1761fa48366e4
Mahmoud abdelaziz <60355133+mahmoudabdelaziz533@users.noreply.github.com>
2023-03-16 20:27:42 +0200

    Update and rename 04_missing_data.py to missing_data.py

--- missing_data.py
+++ missing_data.py
@@ -15,15 +15,8 @@ import pandas as pd
 df = pd.DataFrame({'A':[1,2,np.nan],
                   'B':[5,np.nan,np.nan],
                   'C':[1,2,3]})
-
-df
-
 df.dropna()
-
 df.dropna(axis=1)
-
 df.dropna(thresh=2)
-
 df.fillna(value='FILL VALUE')
-
 df['A'].fillna(value=df['A'].mean())
